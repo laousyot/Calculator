b837034f0ffea1910c538ac90a0ad6bc9ce7229f
Othmane Laousy <othmany17@hotmail.com>
2013-02-11 22:57:49 -0500

    calculator

--- Calculator/OTLViewController.h
+++ Calculator/OTLViewController.h
@@ -8,6 +8,40 @@
 
 #import <UIKit/UIKit.h>
 
-@interface OTLViewController : UIViewController
+
+@interface OTLViewController : UIViewController{
+    IBOutlet UILabel *displayer;
+    double number1;
+	char operator;
+	bool update;
+	bool clicoperator;
+    
+    int numberNeg;
+}
+
+@property (nonatomic,retain) UILabel *displayer;
+@property(readwrite) double number1;
+@property(readwrite) char  operator;
+@property(readwrite) bool  update;
+@property(readwrite) bool  clicoperator;
+
+
+// Numbers
+- (IBAction) pushNumber:	(id) sender;
+- (IBAction) pushNeg:   (id)sender;
+- (IBAction) pushPoint:	(id) sender;
+
+// Mathematical operators
+- (IBAction) pushPlus:		(id) sender;
+- (IBAction) pushMinus:		(id) sender;
+- (IBAction) pushMult:		(id) sender;
+- (IBAction) pushDiv:		(id) sender;
+- (void) calcul;
+
+// Calculus
+- (IBAction) pushEqual:		(id) sender;
+- (IBAction) pushC:		(id) sender;
+
+
 
 @end
--- Calculator/OTLViewController.m
+++ Calculator/OTLViewController.m
@@ -14,6 +14,8 @@
 
 @implementation OTLViewController
 
+@synthesize displayer, operator, clicoperator, update, number1;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -21,6 +23,13 @@
     // View
     UIImage *patternImage= [UIImage imageNamed:@"arches.png"];
     self.view.backgroundColor=[UIColor colorWithPatternImage:patternImage];
+    number1 = 0;
+	operator = ' ';
+	clicoperator = NO;
+	update = NO;
+	displayer.text = @"0";
+    
+    numberNeg=0;
 
 }
 
@@ -29,5 +38,187 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+/**
+ * Method invoqued when we click on a number
+ */
+- (IBAction) pushNumber:	(id) sender {
+	if (update) {
+		update = NO;
+		displayer.text = [sender currentTitle];
+	}
+	else {
+		if ([displayer.text compare:@"0"]) {
+			displayer.text = [displayer.text stringByAppendingString:[sender currentTitle]];
+		}
+		else {
+			displayer.text = [sender currentTitle];
+		}
+	}
+}
+
+/**
+ * Method invoqued when we click on point
+ */
+- (IBAction) pushPoint:	(id) sender {
+	if ([displayer.text rangeOfString:@"."].location == NSNotFound ) {
+		displayer.text = [displayer.text stringByAppendingString:@"."];
+	}
+}
+- (IBAction) pushNeg: (id)sender{
+    if ([displayer.text rangeOfString:@"+/-"].location == NSNotFound ) {
+        numberNeg++;
+		displayer.text = @"-";
+	}
+
+    
+}
+
+/**
+ * Method invoqued when we click on "+". This method calls the calcul method
+*/
+- (IBAction) pushPlus:		(id) sender {
+	if (clicoperator) {
+		[self calcul];
+	}
+	else {
+		number1 = [displayer.text doubleValue];
+		clicoperator = YES;
+	}
+	operator = '+';
+	update = YES;
+}
+
+/**
+ * Method invoqued when we click on "-". This method calls the calcul method
+ */
+- (IBAction) pushMinus:		(id) sender {
+	if (clicoperator) {
+		[self calcul];
+	}
+	else {
+		number1 = [displayer.text doubleValue];
+		clicoperator = YES;
+	}
+	operator = '-';
+	update = YES;
+}
+
+/**
+ * Method invoqued when we click on "*". This method calls the calcul method
+ */
+- (IBAction) pushMult:		(id) sender {
+	if (clicoperator) {
+		[self calcul];
+	}
+	else {
+		number1 = [displayer.text doubleValue];
+		clicoperator = YES;
+	}
+	operator = '*';
+	update = YES;
+}
+
+/**
+ * Method invoqued when we click on "/". This method calls the calcul method
+ */
+- (IBAction) pushDiv:		(id) sender {
+	if (clicoperator) {
+		[self calcul];
+	}
+	else {
+		number1 = [displayer.text doubleValue];
+		clicoperator = YES;
+	}
+	operator = '/';
+	update = YES;
+}
+
+/**
+ * Method invoqued when we click on the equal. This method calls the calcul method
+ */
+- (IBAction) pushEqual:		(id) sender {
+	[self calcul];
+	update = YES;
+	clicoperator = NO;
+}
+
+/**
+ * Reinitializes the Calculator
+ */
+- (IBAction) pushC:			(id) sender {
+	clicoperator = false;
+	update = true;
+	number1 = 0;
+	operator = ' ';
+	displayer.text = @"0";
+}
+/**
+ * Method that does the calculus
+ */
+- (void) calcul {
+    if (numberNeg % 2 != 0){
+        if (operator == '+') {
+            number1 = number1 + [displayer.text doubleValue];
+            number1= (-1)*number1;
+            displayer.text = [NSString stringWithFormat:@"%g", number1];
+        }
+        
+        if (operator == '-') {
+            number1 = number1 - [displayer.text doubleValue];
+            number1= (-1)*number1;
+            displayer.text = [NSString stringWithFormat:@"%g", number1];
+        }
+        
+        if (operator == '*') {
+            number1 = number1 * [displayer.text doubleValue];
+            number1= (-1)*number1;
+            displayer.text = [NSString stringWithFormat:@"%g", number1];
+        }
+        
+        if (operator == '/') {
+            if ([displayer.text doubleValue] != 0 ){
+                number1 = number1 / [displayer.text doubleValue];
+                number1= (-1)*number1;
+                displayer.text = [NSString stringWithFormat:@"%g", number1];
+                
+            } else {
+                number1 = 0;
+                displayer.text = [NSString stringWithFormat:@"NaN"];
+            }
+            
+        }
+        numberNeg=0;
+    }
+    else {
+        if (operator == '+') {
+            number1 = number1 + [displayer.text doubleValue];
+            displayer.text = [NSString stringWithFormat:@"%g", number1];
+        }
+        
+        if (operator == '-') {
+            number1 = number1 - [displayer.text doubleValue];
+            displayer.text = [NSString stringWithFormat:@"%g", number1];
+        }
+        
+        if (operator == '*') {
+            number1 = number1 * [displayer.text doubleValue];
+            displayer.text = [NSString stringWithFormat:@"%g", number1];
+        }
+        
+        if (operator == '/') {
+            if ([displayer.text doubleValue] != 0 ){
+                number1 = number1 / [displayer.text doubleValue];
+                displayer.text = [NSString stringWithFormat:@"%g", number1];
+                
+            } else {
+                number1 = 0;
+                displayer.text = [NSString stringWithFormat:@"NaN"];
+            }
+        }
+    }
+    
+	
+}
+
 
 @end
